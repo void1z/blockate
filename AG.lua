f1b2df3e92c3018c11a4c682a9b85cb02ee3f03f
void1z <140825309+void1z@users.noreply.github.com>
2023-09-05 16:15:12 +0100

    Update AG.lua

--- AG.lua
+++ AG.lua
@@ -212,10 +212,17 @@ while wait(1) do
         end
     end)
 end
--- Function to open the logs GUI
 local function openLogsGUI()
     Players.LocalPlayer.PlayerGui:WaitForChild("MainGUI"):WaitForChild("Logs").Visible = true
 end
+-- Event handler to check if Logs GUI is closed and reopen it
+local function checkLogsGUI()
+    while wait(4) do
+        local logsGUI = Players.LocalPlayer.PlayerGui:FindFirstChild("MainGUI") and Players.LocalPlayer.PlayerGui.MainGUI.Logs
+        if not logsGUI or not logsGUI.Visible then
+            openLogsGUI()
+        end
+    end
 end
 -- Spawn the event handler in a new thread
 task.spawn(checkLogsGUI)
